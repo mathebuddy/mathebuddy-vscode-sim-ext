2590ff02b4245328f83448ed624a9d1ab8252c83
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2022-10-19 12:17:03 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -10,5 +10,11 @@ npm install
 cp node_modules/@mathebuddy/mathebuddy-compiler/build/mathebuddy-compiler.min.js media/
 cp node_modules/@mathebuddy/mathebuddy-simulator/build/mathebuddy-simulator.min.js media/
 cp node_modules/bootstrap/dist/css/bootstrap.min.css media/
-cp node_modules/@fortawesome/fontawesome-free/css/all.min.css media/fontawesome.min.css
+
+mkdir -p media/fontawesome-free/css
+mkdir -p media/fontawesome-free/webfonts
+cp -r node_modules/@fortawesome/fontawesome-free/css/all.min.css media/fontawesome-free/css/
+cp -r node_modules/@fortawesome/fontawesome-free/webfonts/*.woff2 media/fontawesome-free/webfonts/
+
+npm run compile
 vsce package --out "../mathebuddy-downloads/vscode-extensions/"
