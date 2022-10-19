47dfd1a2ca8c6c0896859e94ac2246c208da6168
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2022-10-19 11:18:43 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,5 +6,9 @@
 # It is also necessary to compile mathebuddy dependencies "mathebuddy-compiler" and "mathebuddy-simulator" locally!
 # TODO: publish npm packages
 
+npm install
+cp node_modules/@mathebuddy/mathebuddy-compiler/build/mathebuddy-compiler.min.js media/
 cp node_modules/@mathebuddy/mathebuddy-simulator/build/mathebuddy-simulator.min.js media/
+cp node_modules/bootstrap/dist/css/bootstrap.min.css media/
+cp node_modules/@fortawesome/fontawesome-free/css/all.min.css media/fontawesome.min.css
 vsce package --out "../mathebuddy-downloads/vscode-extensions/"
